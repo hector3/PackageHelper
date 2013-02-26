d3b8bbd9273dc257f80da8e53dc1cf523a5c56ad
Justin <zzhouj@yahoo.com>
2013-02-26 11:44:11 +0800

    /list (list package infos)

--- src/com/android/packagehelper/WebService.java
+++ src/com/android/packagehelper/WebService.java
@@ -1,12 +1,21 @@
 package com.android.packagehelper;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.util.Log;
+
+import com.android.packagehelper.NanoHTTPD.Response;
 
 public class WebService extends Service {
 
@@ -14,6 +23,7 @@ public class WebService extends Service {
 	private static final int PORT = 8787;
 
 	private NanoHTTPD mHttpd;
+	private PackageManager mPackageManager;
 
 	@Override
 	public void onCreate() {
@@ -24,13 +34,54 @@ public class WebService extends Service {
 				@Override
 				public Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
 					Log.d(TAG, method + " " + uri);
-					// TODO
+					if (method.equals("GET")) {
+						if (uri.equals("/")) {
+							return getHome();
+						} else if (uri.equals("/list")) {
+							return getList();
+						} else if (uri.equals("/icon")) {
+							return getIcon(parms.getProperty("p"));
+						}
+					}
 					return null;
 				}
 			};
+			mPackageManager = getPackageManager();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected Response getIcon(String property) {
+		// TODO
+		return null;
+	}
+
+	protected Response getList() {
+		List<PackageInfo> packageInfos = mPackageManager.getInstalledPackages(0);
+		try {
+			JSONArray rootJson = new JSONArray();
+			for (PackageInfo packageInfo : packageInfos) {
+				JSONObject json = new JSONObject();
+				json.put("packageName", packageInfo.packageName);
+				json.put("versionCode", packageInfo.versionCode);
+				json.put("versionName", packageInfo.versionName);
+				json.put("uid", packageInfo.applicationInfo.uid);
+				json.put("publicSourceDir", packageInfo.applicationInfo.publicSourceDir);
+				json.put("sourceDir", packageInfo.applicationInfo.sourceDir);
+				json.put("label", packageInfo.applicationInfo.loadLabel(mPackageManager).toString());
+				rootJson.put(json);
+			}
+			return mHttpd.new Response(NanoHTTPD.HTTP_OK, "text/json", rootJson.toString(2));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected Response getHome() {
+		// TODO
+		return null;
 	}
 
 	@Override
